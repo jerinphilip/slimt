1dcdcbde4c95e0ce5887f814d40d0ad22d776aee
Jerin Philip <jerinphilip@live.in>
2023-08-13 23:08:42 +0530

    Fix path from CI

--- scripts/ci/macos/03-test.sh
+++ scripts/ci/macos/03-test.sh
@@ -4,8 +4,8 @@ python3 -m pip install bergamot -f https://github.com/jerinphilip/bergamot-trans
 # Download en-de-tiny and de-en-tiny models.
 bergamot download -m en-de-tiny
 
-BROWSERMT=$HOME/.local/share/bergamot/models/browsermt
-PREFIX=$BROWSERMT/ende.student.tiny11
+BROWSERMT="$HOME/Library/Application Support/bergamot/models/browsermt/"
+PREFIX="$BROWSERMT/ende.student.tiny11"
 
 MODEL=model.intgemm.alphas.bin
 VOCAB=vocab.deen.spm
