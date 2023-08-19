686c0c75d0f8692c1320a8303fb4ccdf640a5988
George Tom <geog3tom@gmail.com>
2023-08-20 00:44:53 +0530

    added neon ops except for exp

--- slimt/simd/neon.h
+++ slimt/simd/neon.h
@@ -1,12 +1,14 @@
 
 #include <arm_neon.h>
+#include <cstddef>
 
 namespace slimt {
 
-template <>
-struct VDatum<VExt::w4> {
+template <> struct VDatum<VExt::w4> {
  public:
+  using Scalar = float;
   using Register = float32x4_t;
+  static constexpr size_t kWidth = 4;
   VDatum() = default;
   // NOLINTBEGIN
   VDatum(const Register& register_value) : register_(register_value) {}
@@ -25,7 +27,7 @@ struct VDatum<VExt::w4> {
   }
 
   float operator[](size_t i) const {
-    return *(((float*)&register_) +
+    return *(((float *)&register_) +
              i);  // potentially undefined, but efficient. In
                   // practice __m128 is an array of floats
   }
@@ -37,4 +39,44 @@ struct VDatum<VExt::w4> {
   Register register_;
 };
 
-}  // namespace slimt
+template <enum VExt> struct Ops;
+
+template <> struct Ops<VExt::w4> {
+  using Datum = VDatum<VExt::w4>;
+  using Scalar = Datum::Scalar;
+  using Register = Datum::Register;
+
+  // clang-format off
+  static Datum exp(const Datum& x)                     { return exp256_ps(x); }
+  static Datum relu(const Datum& x)                    { return max(0.0F, x); }
+
+  static Datum max(const Datum& lhs, const Datum& rhs) { return vmaxq_f32(lhs, rhs);}
+  static Datum sub(const Datum& lhs, const Datum& rhs) { return vsubq_f32(lhs, rhs); }
+  static Datum add(const Datum& lhs, const Datum& rhs) { return vaddq_f32(lhs, rhs); }
+  static Datum mul(const Datum& lhs, const Datum& rhs) { return vmulq_f32(lhs, rhs); }
+  static Datum div(const Datum& lhs, const Datum& rhs) { return vdivq_f32(lhs, rhs); }
+  // clang-format on
+
+  static Datum sigmoid(const Datum &x) {
+    Datum e = exp(x);
+    return div(e, add(1.0F, e));
+  }
+
+  struct Reduce {
+    static Scalar max(const Datum &x) {
+      Scalar accumulator = x[0];
+      for (size_t i = 1; i < Datum::kWidth; ++i) {
+        accumulator = accumulator > x[i] ? accumulator : x[i];
+      }
+      return accumulator;
+    }
+    static Scalar sum(const Datum &x) {
+      Scalar accumulator = x[0];
+      for (size_t i = 1; i < Datum::kWidth; ++i) {
+        accumulator += x[i];
+      }
+      return accumulator;
+    }
+  };
+};
+} // namespace slimt
