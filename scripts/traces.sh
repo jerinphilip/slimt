cfb6fdd0d3f8e11b6b6b26bbbaaf2fe1490ea988
Jerin Philip <jerinphilip@live.in>
2023-08-20 14:54:07 +0530

    Add trace generating script

---
+++ scripts/traces.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TAGS=(
+  ende
+  enfr
+)
+
+set -x
+export INTGEMM_CPUID=AVX512VNNI
+BERGAMOT="/home/jerin/code/bergamot-translator/build"
+BROWSERMT="/home/jerin/.local/share/bergamot/models/browsermt"
+mkdir -p traces
+
+for tag in ${TAGS[@]}; do
+  export DEBUG_VARIABLES_SAVE_PATH="/home/jerin/code/slimt/blobs/$tag"
+  rm -r $DEBUG_VARIABLES_SAVE_PATH
+  mkdir -p $DEBUG_VARIABLES_SAVE_PATH
+  $BERGAMOT/app/bergamot --model-config-paths $BROWSERMT/$tag.student.tiny11/config.bergamot.yml --log-level off \
+    < data/numbers3x2.txt \
+    &> traces/$tag.txt
+done
