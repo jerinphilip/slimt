94636c2f5e4bea3249cdcf30cc33894f7f7711ee
Jerin Philip <jerinphilip@live.in>
2023-10-03 20:11:34 +0530

    Separate train and inference

--- cmake/FindSentencePiece.cmake
+++ cmake/FindSentencePiece.cmake
@@ -6,7 +6,18 @@ find_path(
   SentencePiece_INCLUDE_DIR
   NAMES sentencepiece_processor.h
   PATHS ${PC_SentencePiece_INCLUDE_DIRS}
-  PATH_SUFFIXES sentencepiece_processor)
+  PATH_SUFFIXES sentencepiece)
+
+find_library(
+  SentencePiece_TRAIN_LIB
+  NAMES sentencepiece_train
+  PATHS ${PC_SentencePiece_LIBRARY_DIRS}
+  PATH_SUFFIXES sentenecepiece)
+find_library(
+  SentencePiece_INFERENCE_LIB
+  NAMES sentencepiece
+  PATHS ${PC_SentencePiece_LIBRARY_DIRS}
+  PATH_SUFFIXES sentenecepiece)
 
 set(SentencePiece_VERSION ${PC_SentencePiece_VERSION})
 
@@ -26,6 +37,20 @@ endif()
 if(SentencePiece_FOUND AND NOT TARGET SentencePiece::SentencePiece)
   add_library(SentencePiece::SentencePiece INTERFACE IMPORTED)
   set_target_properties(
-    SentencePiece::SentencePiece PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                            "${SentencePiece_INCLUDE_DIR}")
+    SentencePiece::SentencePiece
+    PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${SentencePiece_INCLUDE_DIR}"
+               INTERFACE_LINK_LIBRARIES "${SentencePiece_INFERENCE_LIB}"
+               INTERFACE_LINK_OPTIONS "${PC_SentencePiece_LDFLAGS}")
+
+  add_library(SentencePiece::Train INTERFACE IMPORTED)
+  set_target_properties(
+    SentencePiece::Train
+    PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${SentencePiece_INCLUDE_DIR}"
+               INTERFACE_LINK_LIBRARIES "${SentencePiece_TRAIN_LIB}"
+               INTERFACE_LINK_OPTIONS "${PC_SentencePiece_LDFLAGS}")
+  message(
+    STATUS
+      "${SentencePiece_INCLUDE_DIR} ${SentencePiece_TRAIN_LIB} ${SentencePiece_INFERENCE_LIB}"
+  )
+
 endif()
