d3c26fc05207e76a8d9d325d280a77fff6cbfcc0
Jerin Philip <jerinphilip@live.in>
2023-10-05 18:08:06 +0530

    Use AVX2 for GH runners

--- scripts/ci/ubuntu/02-build.sh
+++ scripts/ci/ubuntu/02-build.sh
@@ -5,11 +5,11 @@ set -eo pipefail
 ARGS=(
   -DWITH_INTGEMM=OFF
   -DWITH_RUY=OFF
-  -DWITH_GEMMOLOGY=ON -DUSE_AVX512=ON
+  -DWITH_GEMMOLOGY=ON -DUSE_AVX2=ON
 )
 
 # Configure
-cmake -B build -S $PWD -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ${ARGS[@]}
+cmake -B build -S $PWD -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "${ARGS[@]}"
 
 # Build
 cmake --build build --target all
