b4c715a9e64617ba7b9ce808d520be1733f040f8
Jerin Philip <jerinphilip@live.in>
2023-10-05 18:26:28 +0530

    Try master

--- scripts/ci/ubuntu/01-setup.sh
+++ scripts/ci/ubuntu/01-setup.sh
@@ -3,4 +3,7 @@
 set -x
 sudo apt-get update
 sudo apt-get install -y build-essential cmake
-sudo apt-get install -y libxsimd-dev
+# sudo apt-get install -y libxsimd-dev
+
+git clone https://github.com/xtensor-stack/xsimd
+(cd xsimd && cmake -DCMAKE_INSTALL_PREFIX=/usr/ && make install)
