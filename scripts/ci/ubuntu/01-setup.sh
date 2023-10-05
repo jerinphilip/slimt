8d1fe69e26378ab713e0ee5356a3804c4a5457e7
Jerin Philip <jerinphilip@live.in>
2023-10-05 20:05:15 +0530

    pin to xsimd 11.1.10

--- scripts/ci/ubuntu/01-setup.sh
+++ scripts/ci/ubuntu/01-setup.sh
@@ -3,7 +3,8 @@
 set -x
 sudo apt-get update
 sudo apt-get install -y build-essential cmake
-# sudo apt-get install -y libxsimd-dev
 
-git clone https://github.com/xtensor-stack/xsimd
-(cd xsimd && cmake -DCMAKE_INSTALL_PREFIX=/usr/ && sudo make install)
+XSIMD_VERSION="11.1.0"
+wget https://github.com/xtensor-stack/xsimd/archive/refs/tags/${XSIMD_VERSION}.tar.gz -O xsimd.tar.gz
+tar xf xsimd.tar.gz
+(cd xsimd-${XSIMD_VERSION} && cmake -DCMAKE_INSTALL_PREFIX=/usr/ && sudo make install)
