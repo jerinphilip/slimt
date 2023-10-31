51593bf73a39d179e8d213296617700b760d95b0
Jerin Philip <jerinphilip@live.in>
2023-11-01 00:43:08 +0530

    build out of source

--- scripts/ci/ubuntu/01-setup.sh
+++ scripts/ci/ubuntu/01-setup.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y build-essential cmake
 XSIMD_VERSION="11.1.0"
 wget https://github.com/xtensor-stack/xsimd/archive/refs/tags/${XSIMD_VERSION}.tar.gz -O xsimd.tar.gz
 tar xf xsimd.tar.gz
-(cd xsimd-${XSIMD_VERSION} && cmake -DCMAKE_INSTALL_PREFIX=/usr/ && sudo make install)
+(cd xsimd-${XSIMD_VERSION} && cmake -DCMAKE_INSTALL_PREFIX=/usr/ -B build -S . && sudo make install)
 
 lscpu
 
