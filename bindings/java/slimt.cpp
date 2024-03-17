98647df9c79a44885d6daad807e3374e383590c6
Jerin Philip <jerinphilip@live.in>
2024-03-18 01:15:44 +0530

    Add package relay code Java -> Native

--- bindings/java/slimt.cpp
+++ bindings/java/slimt.cpp
@@ -49,8 +49,39 @@ JNIEXPORT jlong SLIMT_JNI_EXPORT(Model, createModel)(JNIEnv *env, jobject obj,
   // Extract Package object fields
   // Assuming Package object contains necessary fields for Model creation
 
+  jclass package_cls = env->GetObjectClass(jpackage);
+  jfieldID model_field =
+      env->GetFieldID(package_cls, "model", "Ljava/lang/String;");
+  jfieldID vocabulary_field =
+      env->GetFieldID(package_cls, "vocabulary", "Ljava/lang/String;");
+  jfieldID shortlist_field =
+      env->GetFieldID(package_cls, "shortlist", "Ljava/lang/String;");
+  jfieldID ssplit_field =
+      env->GetFieldID(package_cls, "ssplit", "Ljava/lang/String;");
+
+  jstring j_model = (jstring)env->GetObjectField(jpackage, model_field);
+  jstring j_vocabulary =
+      (jstring)env->GetObjectField(jpackage, vocabulary_field);
+  jstring j_shortlist = (jstring)env->GetObjectField(jpackage, shortlist_field);
+  jstring j_ssplit = (jstring)env->GetObjectField(jpackage, ssplit_field);
+
+  const char *model_cstr = env->GetStringUTFChars(j_model, nullptr);
+  const char *vocabulary_cstr = env->GetStringUTFChars(j_vocabulary, nullptr);
+  const char *shortlist_cstr = env->GetStringUTFChars(j_shortlist, nullptr);
+  const char *ssplit_cstr = env->GetStringUTFChars(j_ssplit, nullptr);
+
   // Create Package object
-  slimt::Package<std::string> package;  // Assuming package type is std::string
+  slimt::Package<std::string> package;
+  package.model = std::string(model_cstr);
+  package.vocabulary = std::string(vocabulary_cstr);
+  package.shortlist = std::string(shortlist_cstr);
+  package.ssplit = std::string(ssplit_cstr);
+
+  // Release Java string references
+  env->ReleaseStringUTFChars(j_model, model_cstr);
+  env->ReleaseStringUTFChars(j_vocabulary, vocabulary_cstr);
+  env->ReleaseStringUTFChars(j_shortlist, shortlist_cstr);
+  env->ReleaseStringUTFChars(j_ssplit, ssplit_cstr);
 
   // Create Model object
   slimt::Model *model = new slimt::Model(config, package);
