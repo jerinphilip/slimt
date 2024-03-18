1c54a7027f8140b44c987385fa0fd13338f6cd84
Jerin Philip <jerinphilip@live.in>
2024-03-18 17:14:33 +0530

    Updates to android packaging?

--- scripts/ci/android/04-package.sh
+++ scripts/ci/android/04-package.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+cd bindings/java
+
+# Compile Java files
+javac com/github/jerinphilip/slimt/*.java
+
+# Package Java class files into JAR
+jar cf slimt.jar com/github/jerinphilip/slimt/*.class
+
+# Structure your JAR file
+mkdir -p com/github/jerinphilip/slimt/jni/android-arm64-v8a
+
+# Copy compiled JNI libraries into the JAR
+# Package everything into final JAR file
+cp build/bindings/java/libslimt_jni.so com/github/jerinphilip/slimt/jni/android-arm64-v8a/libslimt_jni.so
+jar uf slimt.jar com/github/jerinphilip/slimt/jni/android-arm64-v8a/libslimt_jni.so
