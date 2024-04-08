0c8116e167ead36b81232bf2401c091cbf5c6b8d
Jerin Philip <jerinphilip@live.in>
2024-04-08 15:53:52 +0530

    Update build script to use io instead of com

--- scripts/ci/android/04-package.sh
+++ scripts/ci/android/04-package.sh
@@ -3,15 +3,15 @@
 cd bindings/java
 
 # Compile Java files
-javac com/github/jerinphilip/slimt/*.java
+javac io/github/jerinphilip/slimt/*.java
 
 # Package Java class files into JAR
-jar cf slimt.jar com/github/jerinphilip/slimt/*.class
+jar cf slimt.jar io/github/jerinphilip/slimt/*.class
 
 # Structure your JAR file
-mkdir -p com/github/jerinphilip/slimt/jni/android-arm64-v8a
+mkdir -p io/github/jerinphilip/slimt/jni/android-arm64-v8a
 
-# Copy compiled JNI libraries into the JAR
+# Copy iopiled JNI libraries into the JAR
 # Package everything into final JAR file
-cp ../../build/bindings/java/libslimt_jni.so com/github/jerinphilip/slimt/jni/android-arm64-v8a/libslimt_jni.so
-jar uf slimt.jar com/github/jerinphilip/slimt/jni/android-arm64-v8a/libslimt_jni.so
+cp ../../build/bindings/java/libslimt_jni.so io/github/jerinphilip/slimt/jni/android-arm64-v8a/libslimt_jni.so
+jar uf slimt.jar io/github/jerinphilip/slimt/jni/android-arm64-v8a/libslimt_jni.so
