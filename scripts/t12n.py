29cc579ad170c440c30789e5d0cc355e306707e7
Jerin Philip <jerinphilip@live.in>
2024-04-09 15:46:24 +0530

    Sent line broken data to t12n

--- scripts/t12n.py
+++ scripts/t12n.py
@@ -33,7 +33,8 @@ if __name__ == "__main__":
     model_nano = Model(nano, package)
 
     data = sys.stdin.read()
-    responses = service.translate(model_nano, [data], html=False)
+    lines = data.splitlines()
+    responses = service.translate(model_nano, lines, html=False)
 
     for response in responses:
         print(response.source.text, "->", response.target.text)
